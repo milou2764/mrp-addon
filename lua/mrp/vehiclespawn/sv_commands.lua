f4ede7682499bff4ecc7fa102f3c0102a570206d
milou2764 <emilien.andre@idakto.com>
2024-10-07 18:00:20 +0100

    fix vehicle spawn system

--- lua/mrp/vehiclespawn/sv_commands.lua
+++ lua/mrp/vehiclespawn/sv_commands.lua
@@ -1,11 +1,11 @@
 MRP.Commands.vehic = {}
-local cat = "vehic"
+local spawnCat = "vehic"
 MRP.Commands.vehic.add = function(ply, class)
     local map = game.GetMap()
     if not MRP.Spawns then MRP.Spawns = {} end
     if not MRP.Spawns[map] then MRP.Spawns[map] = {} end
-    if not MRP.Spawns[map][cat] then
-        MRP.Spawns[map][cat] = {}
+    if not MRP.Spawns[map][spawnCat] then
+        MRP.Spawns[map][spawnCat] = {}
     end
 	local cat
 	if string.find(class, "simfphys") or string.find(class, "sim_fphys") then
@@ -14,11 +14,11 @@ MRP.Commands.vehic.add = function(ply, class)
 		cat = "wac"
 	end
     table.insert(
-		MRP.Spawns[map][cat],
+		MRP.Spawns[map][spawnCat],
 		{
 			pos = ply:GetPos(),
-		        ang = ply:GetAngles(),
-                        class = class,
+            ang = ply:GetAngles(),
+            class = class,
 			cat = cat,
 		}
 	)
@@ -26,5 +26,5 @@ MRP.Commands.vehic.add = function(ply, class)
     ply:ChatPrint(class .. " spawn added")
 end
 
-MRP.Commands.vehic.list = function(ply) MRP.ListSpawns(ply, cat) end
-MRP.Commands.vehic.rm = function(ply, id) MRP.RemoveSpawn(cat, id) end
+MRP.Commands.vehic.list = function(ply) MRP.ListSpawns(ply, spawnCat) end
+MRP.Commands.vehic.rm = function(ply, id) MRP.RemoveSpawn(spawnCat, id) end
