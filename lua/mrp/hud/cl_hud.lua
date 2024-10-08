2457010202c2aa9d383a116f2248edaac841ac07
milou2764 <emilien.andre@idakto.com>
2024-10-08 13:49:40 +0100

    hid ammo and weapon pickup notification

--- lua/mrp/hud/cl_hud.lua
+++ lua/mrp/hud/cl_hud.lua
@@ -6,8 +6,16 @@ local hide = {
 }
 RunConsoleCommand("mp_show_voice_icons", "0")
 
-hook.Add( "HUDShouldDraw", "MRP_HUDShouldDraw", function( name )
+hook.Add("HUDShouldDraw", "MRP_HUDShouldDraw", function(name)
 	if hide[name] then
 		return false
 	end
-end )
+end)
+
+hook.Add("HUDDrawPickupHistory", "MRP_HUDDrawPickupHistory", function()
+	return true 
+end)
+
+hook.Add( "HUDItemPickedUp", "MRP_HUDItemPickedUp", function()
+    return true
+end)
